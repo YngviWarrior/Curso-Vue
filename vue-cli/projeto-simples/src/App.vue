3aa4eac4422735d4faf5c71b4a654920509780b3
root <igorasft@gmail.com>
2020-12-07 21:58:56 -0300

    Fim do modulo 6

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo += '##'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Teste Data usando Vue JS!!'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
