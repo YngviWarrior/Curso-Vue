33c0ee3ed7de35458469060544df96f355d18885
YngviWarrior <igorasft@gmail.com>
2021-01-09 23:25:01 -0300

    salvando

---
+++ 14.Transições/animacoes-exercicios/src/App.vue
@@ -0,0 +1,109 @@
+<template>
+	<div id="app" class="container-fluid">
+		<h1>Animações</h1>
+		<hr>
+		<!-- o atributo variant aplica coloração do css Bootstrap. -->
+		<b-button variant="primary" class="mb-4"
+			@click="exibir = !exibir">Mostrar Mensagem</b-button>
+		
+		<!-- nome da classe -->
+		<!-- <transition name="fade" appear>
+			<b-alert variant="info" show v-if="exibir"> {{ msg }} </b-alert>
+		</transition> -->
+		<!-- type, irá priorizar o tempo da animação ou transição, caso 1 seja maior -->
+		<!-- <transition name="slide" type="animation" appear>
+			<b-alert variant="info" show v-show="exibir"> {{ msg }} </b-alert>
+		</transition> -->
+		<!-- Classes importadas por link, no index, 
+			estamos usando nomeclaturas fora do padrão vue -->
+		<!-- <transition
+			enter-active-class="animated bounce"
+			leave-active-class="animated shake"
+		>
+			<b-alert variant="info" show v-show="exibir"> {{ msg }} </b-alert>
+		</transition> -->
+		<hr>
+
+		<b-select v-model="tipoAnimacao" class="mb-4">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+		<!-- Necessario o atributo key para funcionar a transição. -->
+		<!-- mode="out-in" tira o elemento e depois entra o outro. -->
+		<transition :name="tipoAnimacao" mode="out-in">
+			<b-alert variant="info" show v-if="exibir" key="info"> {{ msg }} </b-alert>
+			<b-alert variant="warning" show v-else key="warn"> {{ msg }} </b-alert>
+		</transition>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			msg: 'Umas mensagem de informção para o usuário!',
+			exibir: false, // flag para display html
+			/*Se o html, aparecer quando carregar a página, o atributo appear na 
+				tag transition fará a animação e transição inves de aparecer abruptamente.*/
+			tipoAnimacao: 'fade',
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 1.5rem;
+}
+/* Padrão vue: 
+	nome da classe + enter, enter-active, enter-to ou leave, leave-active, leave-to */
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 2s;
+}
+
+/*.fade-enter-to {
+	opacity: 1;
+}
+    Já são valores padrões, saindo de 0 vai p 1
+.fade-leave {
+	opacity: 1;
+}*/
+
+/* Keyframes no eixo y */
+
+@keyframes slide-in {
+	from { transform: translateY(40px);	}
+	to { transform: translateY(0); }
+}
+
+@keyframes slide-out {
+	from { transform: translateY(0); }
+	to { transform: translateY(40px); }
+}
+
+.slide-enter-active {
+	animation: slide-in 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-leave-active {
+	animation: slide-out 2s ease;
+	transition: opacity 2s;
+}
+
+.slide-enter, .slide-leave-to {
+	opacity: 0;
+}
+
+</style>
