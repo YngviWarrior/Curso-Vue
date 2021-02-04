48100ff4d2383e4c7b6075c5f810c97df076319e
YngviWarrior <igorasft@gmail.com>
2021-02-03 21:19:13 -0300

    bug

---
+++ Projeto #03/stock-trader/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-app>
+		<v-container>
+			<router-view></router-view>
+		</v-container>
+	</v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ Projeto #03/stock-trader/src/plugins/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Início</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
